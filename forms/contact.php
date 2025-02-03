10f2ab841828116bd106d5418e4b7802adbb7fb9
Jaypalsinh Barad <jdbarad1010@gmail.com>
2025-02-03 23:51:29 +0530

    Update

--- forms/contact.php
+++ forms/contact.php
@@ -12,7 +12,7 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die('Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
